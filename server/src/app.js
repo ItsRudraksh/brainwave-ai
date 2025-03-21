74486472632213eafe647628dfd9c23f148b1a03
ItsRudraksh <rudrakshkapoor2004@gmail.com>
2025-03-22 01:54:28 +0530

    refactor app.js to comment out deployment-related code and add health check route

--- server/src/app.js
+++ server/src/app.js
@@ -7,22 +7,22 @@ import chatRoutes from "./routes/chat.js";
 import imagekitRoutes from "./routes/imagekit.js";
 import { verifyToken } from "./middleware/verifyToken.js";
 // import { clerkMiddleware } from "@clerk/express";
-import path from "path";
-import url from "url";
+// import path from "path";  //FOR DEPLOYMENT
+// import url from "url";  //FOR DEPLOYMENT
 
 dotenv.config();
 
 const app = express();
 
-const __filename = url.fileURLToPath(import.meta.url); //FOR DEPLOYMENT
-const __dirname = path.dirname(__filename); //FOR DEPLOYMENT
+// const __filename = url.fileURLToPath(import.meta.url); //FOR DEPLOYMENT
+// const __dirname = path.dirname(__filename); //FOR DEPLOYMENT
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({ credentials: true, origin: `${process.env.CLIENT_URL}` }));
 app.use(cookieParser());
 // app.use(clerkMiddleware());
-app.use(express.static(path.join(__dirname, "../../client/dist/"))); //FOR DEPLOYMENT
+// app.use(express.static(path.join(__dirname, "../../client/dist/"))); //FOR DEPLOYMENT
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
@@ -34,9 +34,13 @@ app.get("/api/v1/protected", verifyToken, (req, res) => {
   res.json({ message: "This is a protected route", user: req.user });
 });
 
+app.get("/", (req, res) => {
+  res.send("This is health check route");
+})
+
 //FOR DEPLOYMENT
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
+// });
 
 export default app;

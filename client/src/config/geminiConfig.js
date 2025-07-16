a44847463610554efd738c1d8ebf254e13dd1fb6
ItsRudraksh <rudrakshkapoor2004@gmail.com>
2025-07-17 03:30:54 +0530

    Update .gitignore to include .qodo file; change Gemini model version to 2.5-flash and add _redirects file for routing

--- client/src/config/geminiConfig.js
+++ client/src/config/geminiConfig.js
@@ -6,7 +6,7 @@ import {
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
+  model: "gemini-2.5-flash",
   tools: [
     {
       googleSearch: {},
